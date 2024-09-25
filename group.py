75ddd081bd2b2746ecf2e614f0a3e21a4a5daae6
Mazepa0307 <danekmazepa@gmail.com>
2024-09-25 12:25:56 +0200

    Add files via upload

---
+++ group.py
@@ -0,0 +1,29 @@
+class GroupLimitError(Exception):
+    def __init__(self, message="Cannot add more than 10 students to the group"):
+        self.message = message
+        super().__init__(self.message)
+
+class Group:
+    def __init__(self, number):
+        self.number = number
+        self.group = set()
+
+    def add_student(self, student):
+        if len(self.group) >= 10:
+            raise GroupLimitError()
+        self.group.add(student)
+
+    def find_student(self, last_name):
+        for student in self.group:
+            if student.last_name == last_name:
+                return student
+        return None
+
+    def delete_student(self, last_name):
+        student = self.find_student(last_name)
+        if student:
+            self.group.remove(student)
+
+    def __str__(self):
+        all_students = "\n".join([str(student) for student in self.group])
+        return f"Group: {self.number}\nStudents:\n{all_students}"
